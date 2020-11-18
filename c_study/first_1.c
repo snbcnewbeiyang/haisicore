15f5634feb12a8abff178d3274822cb1d3a2cdfc
NOW <zhougaoxiongcall@163.com>
2020-11-19 00:34:44 +0800

    backup

--- c_study/first_1.c
+++ c_study/first_1.c
@@ -4,10 +4,12 @@
 #define UPPER 300
 #define STEP 20
 
+//摄氏度和温度之间的换算关系  
 void main(void)
 {
 	float fahr,celsius;
 
+//方式1,使用while
 	while(fahr <= UPPER)
 	{
 		celsius = 5.0 * (fahr - 32) / 9.0;
@@ -15,6 +17,8 @@ void main(void)
 		fahr += STEP;
 	}
 	
+//方式2,使用for,更加的紧凑
+//注意在第一个地方必须要写fahr=0,不能不写,不写程序出错
 	for(fahr = 0;fahr <= UPPER;fahr += STEP)
 	{
 		//printf("%3.0f,%6.2f\n",fahr,5.0 * (fahr - 32) /9.0 );
@@ -22,6 +26,7 @@ void main(void)
 
 	printf("%d\n",getchar() != EOF);
 
+//将输入变成输出
 	int c;
 	while((c = getchar()) != EOF)
 	{
---
+++ c_study/first_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0 
+
+//统计输入的行数和字符数,单词数量
+void main(void)
+{
+	int charnum,linenum,worldnum;
+	charnum = linenum = worldnum = 0;
+	int stat = OUT;
+	int c;
+	while(c = getchar() != EOF)
+	{
+		charnum++;
+		
+		if(c == ' ' || c == '\n' || c == '\t')
+		{
+			stat == OUT;
+		}
+		else if(stat == OUT)
+		{
+			stat = IN;
+			++worldnum;
+		}
+
+		if(c == '\n')
+			linenum++;
+	}
+	printf("char num = %d,line num = %d,world num = %d\n",charnum,linenum,worldnum);
+}
