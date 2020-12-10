13414a96193e4d4a16b5e9bd20d12f5f6f30d9ac
zhougaoxiong <zhougaoxiong@newbeiyang.com>
2020-12-10 20:29:24 +0800

    backpppppppp

--- c_study/9_jieogu_struct/test.c
+++ c_study/9_jieogu_struct/test.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+const bb=90;
+
+void fun(char** p,int* len)
+{
+	char* t = (char*)malloc(sizeof(char)*10);
+	*p=t;
+	strcpy(*p,"111111111");
+	*len=strlen(*p);
+}
+
 void main(void)
 {
-	const char b;
-	int a;
-	printf("%c,%d\n",b,a);	
+	char *p = NULL;
+	int len;
+	fun(&p,&len);
+	if(p!=NULL)
+	{
+		printf("%s,%d",p,len);
+	}
 }
---
+++ c_study/9_jieogu_struct/tt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int* fun();
+
+void main(void)
+{
+	int* p=NULL;
+	p=fun();
+//	funn(&p);
+	if(p){
+		printf("-------");
+	}else{
+		printf("+++++++");
+	};
+}
+
+int* fun()
+{
+	int arr[10];
+	return arr;
+//	return (int*)malloc(sizeof(int)*10);
+}
+
+void funn(int** p)
+{
+//	*p=(int*)malloc(sizeof(int)*10);
+	int arr[10];
+	*p=arr;
+}
